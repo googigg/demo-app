126878af2eeb25721484691a3fb1eaf075513d66
“Sowat <sowat.tun@ascendcorp.com>
2018-11-28 11:28:57 +0700

    Exposure hystrix dashboard

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 @SpringBootApplication
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
